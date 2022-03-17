4db84ad317bc779f6e49110fdb2325df14348b5d
Shubham Vadnere <101618357+Shuvadnere@users.noreply.github.com>
2022-03-17 12:42:15 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class swap {
+
+	public static void main(String[] args) {	int a=50,b=40;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println("Now \na= "+a+"\n b="+b);		
+	
+
+}
+
+
+
+	}
+
